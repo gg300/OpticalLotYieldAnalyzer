15a54e1e97442aa49b5a3e0a68e97b3160314f7f
gg300 <georgebot300@yahoo.com>
2026-06-15 11:10:44 +0300

    Created the folder structure with the initial config and mock products

---
+++ OpticalLotYieldAnalyzer/OpticalLotYieldAnalyzer/Config/ProductConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpticalLotYieldAnalyzer;
+
+public class ProductConfig
+{
+    public string ProductId { get; set; } = "";
+    public int LotSize { get; set; }
+    public Dictionary<string, ParameterSpec> Parameters { get; set; } = new();
+}
+
+public class ParameterSpec
+{
+    public double Nominal { get; set; }
+    public double StdDev { get; set; }
+    public double DefectRate { get; set; }
+    public double? Min { get; set; }
+    public double? Max { get; set; }
+    public string Unit { get; set; } = "";
+}
